e9a1ccbf228f5f41e2f6184e8834958c3a98f9e8
JohnChewning <johnathanchewning@gmail.com>
2024-02-15 20:21:06 -0500

    added get to server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const routes = require('./routes/routes');
+const path = require('path'); // Add this line
+const api = require('./routes/routes');
 
 // Initialize the app and create a port
 const app = express();
@@ -8,10 +9,18 @@ const PORT = process.env.PORT || 3001;
 // Set up body parsing, static, and route middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
 app.use(express.static('public'));
 
-// Use the routes defined in routes.js
-app.use('/', routes);
+// GET Route for index.html
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for notes.html (Changed path to '/notes')
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
 // Start the server on the port
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
