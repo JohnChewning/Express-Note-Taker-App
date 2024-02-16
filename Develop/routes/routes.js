14d7e173aafa9f49d2449d02431e8f5e62a8695e
JohnChewning <johnathanchewning@gmail.com>
2024-02-15 20:22:46 -0500

    fixed routes

--- Develop/routes/routes.js
+++ Develop/routes/routes.js
@@ -2,34 +2,27 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 const path = require('path');
+const { readAndAppend, readFromFile } = require('../helper/fsUtils');
 const { v4: uuidv4 } = require('uuid');
 
-// GET /notes route
+// GET /api/notes route
 router.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'public', 'notes.html'));
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// POST /api/notes route
+router.post('/notes', (req, res) => {
+  const newNote = req.body;
+  newNote.id = uuidv4();
+
+  readAndAppend(newNote, './db/db.json');
+
+  res.json(newNote);
 });
 
 // GET * route
 router.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
-});
-
-// GET /api/notes route
-router.get('/api/notes', (req, res) => {
-  const notes = JSON.parse(fs.readFileSync('db.json', 'utf8'));
-  res.json(notes);
-});
-
-// POST /api/notes route
-router.post('/api/notes', (req, res) => {
-  const newNote = req.body;
-  newNote.id = uuidv4();
-
-  const notes = JSON.parse(fs.readFileSync('db.json', 'utf8'));
-  notes.push(newNote);
-
-  fs.writeFileSync('db.json', JSON.stringify(notes));
-  res.json(newNote);
 });
 
 module.exports = router;
